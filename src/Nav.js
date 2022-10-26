17378f86b11a4fe69ff0e9b5abb8d31f5feae4c3
gravender <ss.noah@gmail.com>
2022-10-26 21:15:38 +1100

    Added a basic div and styling to view the cart

--- src/Nav.js
+++ src/Nav.js
@@ -2,20 +2,36 @@ import React from "react";
 import { Link } from "react-router-dom";
 function Nav() {
   const navStyle = {
-    color: "white",
+    color: "#BBB193",
+  };
+  const showCart = () => {
+    let cart = document.getElementById("shoppingCart");
+    if (cart.classList.contains("visible")) {
+      cart.classList.remove("visible");
+    } else {
+      cart.classList.add("visible");
+    }
   };
   return (
-    <nav>
-      <h3>Logo</h3>
-      <ul className="nav-links">
-        <Link style={navStyle} to="/">
-          <li>home</li>
-        </Link>
-        <Link style={navStyle} to="/shop">
-          <li>shop</li>
-        </Link>
-      </ul>
-    </nav>
+    <div>
+      {" "}
+      <nav>
+        <h1 className="navTitle">Shopping Cart</h1>
+        <ul className="nav-links">
+          <Link style={navStyle} to="/">
+            <li>home</li>
+          </Link>
+          <Link style={navStyle} to="/shop">
+            <li>shop</li>
+          </Link>
+          <li onClick={() => showCart()}>cart</li>
+        </ul>
+      </nav>
+      <div id="shoppingCart" className="shoppingCart">
+        <div onClick={() => showCart()} className="blankArea"></div>
+        <div className="Cart"></div>
+      </div>
+    </div>
   );
 }
 
